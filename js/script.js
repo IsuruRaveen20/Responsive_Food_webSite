3968fd86083317e84f88656fc6aeaa76caa52846
Isuru Raveen <it19152592@my.sliit.lk>
2022-01-04 11:54:36 +0530

    search button working and close button working

--- js/script.js
+++ js/script.js
@@ -14,3 +14,7 @@ window.onscroll = () => {
 document.querySelector('#search-icon').onclick = () => {
     document.querySelector('#search-form').classList.toggle('active');
 }
+
+document.querySelector('#close').onclick = () => {
+    document.querySelector('#search-form').classList.remove('active');
+}
