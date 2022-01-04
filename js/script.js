517c237520634a8e90d615d8708ddae389e460d4
Isuru Raveen <it19152592@my.sliit.lk>
2022-01-04 11:49:53 +0530

    search button on click added

--- js/script.js
+++ js/script.js
@@ -5,3 +5,12 @@ menu.onclick = () => {
     menu.classList.toggle('fa-items');
     navbar.classList.toggle('active');
 }
+
+window.onscroll = () => {
+    menu.classList.remove('fa-times');
+    navbar.classList.remove('active');
+}
+
+document.querySelector('#search-icon').onclick = () => {
+    document.querySelector('#search-form').classList.toggle('active');
+}
